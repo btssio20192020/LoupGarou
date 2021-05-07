fcfee3364fe0f873efbdd103a35a33156bd6510a
btssio20192020 <mmelerayd@gmail.com>
2021-05-07 22:20:21 +0200

    Update Client.cs
    Modification de la classe Client

--- LoupGarouTY/LoupGarou/Client.cs
+++ LoupGarouTY/LoupGarou/Client.cs
@@ -12,31 +12,53 @@ namespace LoupGarou
 {
     public class Client
     {
-        Socket leSocket;
-        IPEndPoint leIPEP;
-        TextReader leTR;
-        TextWriter leTW;
-        string lAdresseS;
-        string lAdresseC;
-        int lePortS;
-        Plateforme laPlateforme;
-        string leLogin;
-
-        public Client(string adresseS, int portS, string login)
+        public Socket leSocket;
+        public IPEndPoint leIPEP;
+        public TextReader leTR;
+        public TextWriter leTW;
+        public NetworkStream leNS;
+        public string lAdresseS;
+        public string lAdresseC;
+        public int lePortS;
+        public Plateforme laPlateforme;
+        public string leLogin;
+        public int numero = 0;
+        
+        //Constructeur pour les tests en mode console
+        public Client(int num)
         {
-            leSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            leIPEP = new IPEndPoint(IPAddress.Parse(adresseS), portS);
-            leSocket.Connect(leIPEP);
-
-
+            this.numero = num;
         }
 
+        //Constructeur A partir de la plateforme
+        public Client(string adresseS, int port, string adresseC, int num = 0, Plateforme laPlateforme = null)
+        {
+            
+                this.numero = num;
+                this.lAdresseS = adresseS;
+                this.lAdresseC = adresseC;
+                this.lePortS = port;
+                this.laPlateforme = laPlateforme;
+                this.leSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.leIPEP = new IPEndPoint(IPAddress.Parse(adresseS), port);
+                this.leSocket.Connect(leIPEP);
+                leNS = new NetworkStream(this.leSocket);
+                leTR = new StreamReader(this.leNS);
+                leTW = new StreamWriter(this.leNS);
+        }
+
+        //Constructeur pour le client
+        public Client(string adresseS, string adresseC, int num, TextReader TR, TextWriter TW)
+        {
+            this.lAdresseS = adresseS;
+            this.lAdresseC = adresseC;
+            this.numero = num;
+            this.leTR = TR;
+            this.leTW = TW;
+        }
+        
         public void Run()
         {
-            NetworkStream leNS = new NetworkStream(leSocket);
-            leTR = new StreamReader(leNS);
-            leTW = new StreamWriter(leNS);
-
             Thread leThread = new Thread(EcouteReponse);
             leThread.Start();
         }
